942119aa9162405eb15f98ba7170c5716472692a
farhansherinvm <farhanasherinvm@gmail.com>
2025-10-02 01:24:54 +0530

    production bud

--- backend/settings.py
+++ backend/settings.py
@@ -100,12 +100,23 @@ CLOUDINARY_STORAGE = {
 # Set Cloudinary as the default storage backend for media files
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'zecserbusiness@gmail.com'
+# EMAIL_HOST_PASSWORD = 'qofr etky gfux qfjg'
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+import os
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'zecserbusiness@gmail.com'
-EMAIL_HOST_PASSWORD = 'qofr etky gfux qfjg'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() in ('true', '1')
+
+# IMPORTANT: Read credentials from environment variables
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'zecserbusiness@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'lfqx aljl srkx ttur') 
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
